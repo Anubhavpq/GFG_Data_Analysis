3dd63a52d7159ed8262e9b80eeb5b6c5537fd2d1
Anubhav <goyalanubhavpq@gmail.com>
2026-03-24 18:40:37 +0530

    house prediction

--- GEEKAnalysis/streamlit/data_visualization.py
+++ GEEKAnalysis/streamlit/data_visualization.py
@@ -24,3 +24,54 @@ st.text('2. Bar Plot using Matplotlib')
 fig = plt.figure(figsize= (15,8))
 data['species'].value_counts().plot(kind = 'bar')
 st.pyplot(fig)
+
+st.text('2. histplot using seaborn')
+fig = plt.figure(figsize= (15,8))
+sns.histplot(data['sepal_length'] , kde= True)
+st.pyplot(fig)
+
+col1 , col2 = st.columns(2)
+
+with col1:
+    col1.write('KDE = False')
+    fig1 = plt.figure()
+    sns.distplot(data['sepal_length'], kde = False)
+    st.pyplot(fig1)
+
+with col2:
+    col2.write('Hist = False')
+    fig2 = plt.figure()
+    sns.distplot(data['sepal_length'], hist = False)
+    st.pyplot(fig2)
+
+col1 , col2 = st.columns(2)
+with col1:
+    fig1 = plt.figure()
+    sns.set_style('darkgrid')
+    sns.distplot(data['petal_length'], hist = False)
+    st.pyplot(fig1)
+with col2:
+    fig2 = plt.figure()
+    sns.set_theme (context = 'poster')
+    sns.distplot(data['petal_length'], hist = False)
+    st.pyplot(fig2)
+
+st.text('6. Scatter Plot')
+fig, ax = plt.subplots(figsize =(15,8))
+ax.scatter(*np.random. random(size = (2,100)))
+st.pyplot(fig)
+
+st.text('6. Count Plot')
+fig = plt.figure(figsize = (15,8))
+sns.countplot(data = data, x = 'species' )
+st.pyplot()
+
+st.text('8. Box Plot')
+fig = plt.figure(figsize= (15,8))
+sns.boxplot(data = data, x = 'species', y = 'petal_length')
+st.pyplot(fig)
+
+st.text('9. Violin Plot')
+fig = plt.figure(figsize= (15,8))
+sns.violinplot(data = data , x = 'species', y = 'petal_length')
+st.pyplot(fig)
